9c762b275247227cce2f6c63a92e1ed6c88a6926
joel.jose <joel.jose@FLY-LP305.flytxt.com>
2017-10-24 11:32:12 +0530

    hook class

--- src/test/java/Hooks.java
+++ src/test/java/Hooks.java
@@ -1,7 +1,7 @@
 import cucumber.api.java.After;
 import baseClasses.browserInit;
 public class Hooks extends browserInit{
-
+//after all classes
 	@After
 	public void afterClass(){
 		driver.close();
